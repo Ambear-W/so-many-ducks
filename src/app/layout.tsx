806054001ae727aea86d829441584993b092b6aa
Amber W <43912805+Ambear-W@users.noreply.github.com>
2023-12-14 14:37:13 -0500

    renamed page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Too Many Ducks!',
+  title: 'So Many Ducks!',
   description: 'You are in control of how many ducks there are.',
 }
 
