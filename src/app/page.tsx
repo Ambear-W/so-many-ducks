6fed071658028c817e6938836f06265df0f27b13
Amber W <43912805+Ambear-W@users.noreply.github.com>
2023-12-14 14:52:58 -0500

    updated text alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SummonDuckWrapper from './components/SummonDuckWrapper';
 export default function Home() {
   return (
     <main>
-      <div className='fixed top-[40%] left-[50%] -translate-y-1/2 -translate-x-1/2 text-2xl font-semibold'>
+      <div className='fixed top-[40%] left-[50%] -translate-y-1/2 -translate-x-1/2 text-2xl font-semibold text-center'>
         <p>Spawn ducks until you can't or stop when there's too many ducks!</p>
       </div>
       <SummonDuckWrapper />
